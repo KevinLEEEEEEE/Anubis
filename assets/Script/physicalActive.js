21e0b301e2c38210a65d5bc789bf81fdfd49291d
Kevin <KevinLEEEEEEE@users.noreply.github.com>
2018-07-01 10:14:30 +0800

    player and blocks
    plsyerInput,playerMotions and playerContact fullfilled, along with basic blocks

--- assets/Script/physicalActive.js
+++ assets/Script/physicalActive.js
@@ -7,12 +7,12 @@ export default class physicalActive extends cc.Component {
     onLoad() {
         cc.director.getPhysicsManager().enabled = true;
         cc.director.getPhysicsManager().debugDrawFlags = 
-            // 0;
+            0;
             // cc.PhysicsManager.DrawBits.e_aabbBit |
             // cc.PhysicsManager.DrawBits.e_pairBit |
             // cc.PhysicsManager.DrawBits.e_centerOfMassBit |
-            cc.PhysicsManager.DrawBits.e_jointBit |
-            cc.PhysicsManager.DrawBits.e_shapeBit; // 激活物理组件与debug框
+            // cc.PhysicsManager.DrawBits.e_jointBit |
+            // cc.PhysicsManager.DrawBits.e_shapeBit; // 激活物理组件与debug框
             ls.sets();
             // localStorage.sets();
     }
